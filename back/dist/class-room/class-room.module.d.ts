54c934bc2a109a5088d131abeb0e2ec58afaee7c
hitoshi555 <hitoshi1845@gmail.com>
2024-04-23 22:21:14 +0900

    environment settings for backend

--- back/dist/class-room/class-room.module.d.ts
+++
@@ -1,8 +0,0 @@
-import { ClassRoomController } from './class-room.controller';
-import { ClassRoomService } from './class-room.service';
-import { PrismaModule } from 'src/prisma/prisma.module';
-export declare class ClassRoomModule {
-    controllers: [ClassRoomController];
-    providers: [ClassRoomService];
-    imports: [PrismaModule];
-}
--- front/src/pages/A.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <h1>Component A.</h1>
-  <router-link to="/">
-    home
-  </router-link>
-</template>
